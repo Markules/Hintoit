cd41747e6978dd8aefc1e36ab8ad4e410f4ac3e8
Markules <markys.1988@gmail.com>
2021-01-25 14:40:51 +0200

    Added a profielNavBar component

--- client/src/containers/Profile/ProfileNavBar/ProfileNavBar.js
+++ client/src/containers/Profile/ProfileNavBar/ProfileNavBar.js
@@ -1,13 +1,16 @@
+import { use } from "passport";
 import React from "react";
-
+import { Link } from 'react-router-dom';
 import classes from "./ProfileNavBar.module.css";
 
 const ProfileNavBar = (props) => {
-
+const user = props.userData
 return (
-    <div>
-        profile nav bar
-    </div>
+    <ul className={classes.navContainer}>
+       <li className={classes.listItem}><Link to='followers'><p>Followers {user.followers.length ? (user.followers.length) : 0}</p></Link></li> 
+       <li className={classes.listItem}><Link to='following'>Following {user.following.length ? (user.following.length) : 0}</Link></li>
+        <li className={classes.listItem}><Link to='items'>Items {user.gifts.length ? (user.gifts.length) : 0}</Link></li>
+    </ul>
 )
 }
 
