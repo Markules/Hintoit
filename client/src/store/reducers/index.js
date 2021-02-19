348af78c7b37745c46ff30aaaf20f24999a7cf44
Markules <markys.1988@gmail.com>
2021-02-19 22:37:10 +0200

    addedalert action and reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -2,9 +2,12 @@ import { combineReducers } from 'redux';
 import auth from './auth';
 import profile from './profile';
 import items from './items';
+import alert from './alert';
+
 
 export default combineReducers ({
     auth,
     profile,
     items,
+    alert
 });
