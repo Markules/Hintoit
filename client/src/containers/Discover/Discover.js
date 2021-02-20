586ccf56956297bd018dcfb430e9543d50cf0a5e
Markules <markys.1988@gmail.com>
2021-02-20 23:05:15 +0200

    added list types to discover page

--- client/src/containers/Discover/Discover.js
+++ client/src/containers/Discover/Discover.js
@@ -1,14 +1,53 @@
 import React, { Component } from "react";
 import ItemsList from "../../components/Items/ItemsList/ItemsList";
+import UsersList from "../../components/Users/UsersList/UsersList";
 import Aux from "../../hoc/Aux/Aux";
 
+import classes from "./Discover.module.css";
+
 export class Discover extends Component {
+  state = {
+    list: "items",
+    itemActive: true,
+    UsersActive: false
+  };
+
   render() {
     const cardType = "discover";
+    let listType =
+      this.state.list === "items" ? (
+        <ItemsList cardType={cardType} />
+      ) : (
+        <UsersList />
+      );
+
+    const onChangeItemsList = () => {
+      this.setState({ list: "items", itemActive: true, UsersActive: false });
+    };
+
+    const onChangeUsersList = () => {
+      this.setState({ list: "users", itemActive: false, UsersActive: true });
+    };
+
     return (
       <Aux>
-        Discover
-        <ItemsList cardType={cardType} />
+        <div className={classes.ListTypeContainer}>
+          <ul className={classes.ListTypes}>
+            <li
+              className={[classes.Type, classes.ItemsType, this.state.itemActive ? classes.ItemActive : null].join(" ")}
+              onClick={() => onChangeItemsList()}
+            >
+              ITEMS
+            </li>
+            <li
+              className={[classes.Type, classes.UsersType, this.state.UsersActive ? classes.UsersActive : null].join(" ")}
+              onClick={() => onChangeUsersList()}
+            >
+              USERS
+            </li>
+          </ul>
+        </div>
+        {listType}
       </Aux>
     );
   }
